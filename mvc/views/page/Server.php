c2f36962feb2bc904218a891371cccc734d700c8
vovanthanh95 <vovanthanh180295@gmail.com>
2021-09-16 19:57:22 +0700

    add trading view

--- mvc/views/page/Server.php
+++ mvc/views/page/Server.php
@@ -28,3 +28,34 @@ $(document).ready(function(){
 </script>
 
 <input type="text" id="ffname" name="ffname"><br>
+<!-- TradingView Widget BEGIN -->
+<div class="tradingview-widget-container">
+  <div id="tradingview_915b4"></div>
+  <div class="tradingview-widget-copyright"><a href="https://vn.tradingview.com/symbols/BTCUSDT/?exchange=BINANCE" rel="noopener" target="_blank"><span class="blue-text">BTCUSDT Biểu đồ</span> </a> bởi TradingView</div>
+  <script type="text/javascript" src="https://s3.tradingview.com/tv.js"></script>
+  <script type="text/javascript">
+  new TradingView.widget(
+  {
+  "width": 980,
+  "height": 610,
+  "symbol": "BINANCE:BTCUSDT",
+  "interval": "D",
+  "timezone": "Asia/Bangkok",
+  "theme": "dark",
+  "style": "1",
+  "locale": "vi_VN",
+  "toolbar_bg": "#f1f3f6",
+  "enable_publishing": false,
+  "hide_side_toolbar": false,
+  "allow_symbol_change": true,
+  "save_image": false,
+  "details": true,
+  "studies": [
+    "IchimokuCloud@tv-basicstudies"
+  ],
+  "container_id": "tradingview_915b4"
+}
+  );
+  </script>
+</div>
+<!-- TradingView Widget END -->
